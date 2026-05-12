8532ae57611896c41d75bc541dce69ee36854c60
liulefei1 <liulefei@hzbank.com.cn>
2026-05-12 16:16:05 +0800

    pag 点击 demo

---
+++ shared/src/commonMain/kotlin/com/wwwcg/kuikly/widgetgrid/demo/PAGPage.kt
@@ -0,0 +1,44 @@
+package com.wwwcg.kuikly.widgetgrid.demo
+
+import com.tencent.kuikly.core.annotations.Page
+import com.tencent.kuikly.core.base.ViewBuilder
+import com.tencent.kuikly.core.log.KLog
+import com.tencent.kuikly.core.pager.Pager
+import com.tencent.kuikly.core.timer.setTimeout
+import com.tencent.kuikly.core.views.PAG
+@Page("PAGPage")
+class PAGPage: Pager() {
+    override fun body(): ViewBuilder {
+        return {
+            attr {
+                allCenter()
+            }
+            PAG {
+                attr {
+                    src("blue.pag")
+                    repeatCount(0)
+                    autoPlay(true)
+                    setTimeout(100) {
+                        replaceTextLayerContent(
+                            "text01",
+                            "123"
+                        )
+                    }
+                    replaceTextLayerContent(
+                        "text02",
+                        "456"
+                    )
+                    size(
+                        80f,
+                        80f
+                    )
+                }
+                event {
+                    click {
+                        KLog.e("PAG","pag click")
+                    }
+                }
+            }
+        }
+    }
+}
